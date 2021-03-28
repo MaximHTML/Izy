50852709d45065ac1e5c2ea3fb710fc7599e0b28
Maxim <79743489+MaximHTML@users.noreply.github.com>
2021-03-28 20:37:42 +0300

    g

---
+++ index.php
@@ -0,0 +1,17 @@
+<!Doctype html>
+<html>
+    <head>
+       <meta charset="UTF-8">
+        <title>Авторизация</title>
+        <link rel="stylesheet" href="CSS/main.css">
+    </head>
+    <body>
+        <form action="" method="">
+            <label>Логин</label>
+            <input type="text">
+             <label>Пароль</label>
+            <input type="password">
+            <button>Войти</button>
+        </form>
+    </body>
+</html>
